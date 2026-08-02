63a9c878dbec648794dc6119ed1d8ca5ac84f0a8
Will Robertson <will@wspr.io>
2026-08-02 21:10:01 +0930

    last minute prac1 changes

---
+++ pracs/Prac_1.m
@@ -0,0 +1,49 @@
+%<*partA>
+
+% Note: your naming might be different!
+
+% Update the sine wave block in Simulink programmatically 
+set_param('QubeModel/Sine Wave', ...
+             'Amplitude', '0.5', ...
+             'Bias', '0',...
+             'Frequency', '1*2*pi', ...
+             'Phase', '0' ...
+          );
+
+% Update the transfer function block in Simulink programmatically 
+omega_c = 50;
+set_param('QubeModel/Low-pass filter (rad)', ...
+               'Numerator', sprintf('[%g]', omega_c), ...
+               'Denominator', mat2str([1 omega_c])...
+          );
+%</partA>
+%<*partB>
+
+% Handle the Simulink scope output data structure with one channel
+scopeData = out.ScopeData;
+
+t = scopeData.time;
+y = scopeData.signals.values;
+
+figure;
+plot(t, y);
+grid on
+xlabel('Time, s')
+ylabel('Amplitude')
+%</partB>
+%<*partC>
+% Handle the Simulink scope output data structure with two channels
+scopeData = out.ScopeData;
+
+t = scopeData.time;
+y1 = scopeData.signals(1).values;
+y2 = scopeData.signals(2).values;
+
+figure; 
+plot(t, y1);
+hold on 
+plot(t, y2);
+grid on
+xlabel('Time, s')
+ylabel('Amplitude')
+%</partC>
