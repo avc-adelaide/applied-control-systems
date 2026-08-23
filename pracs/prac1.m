b8839e20e8339a485b23f071db919406ebdaf804
Will Robertson <will@wspr.io>
2026-08-23 19:28:49 +0930

    prac3 updates (thanks Matt)

--- pracs/prac1.m
+++ pracs/prac1.m
@@ -28,7 +28,7 @@ set(model_inertia_disk,'InputName',{'V\_{command}'},...
 step(model_inertia_disk)
 
 % To customise the output, return the variables then manually plot
-% [x,y] = step(model_inertia_disk)
+% [y,tOut] = step(model_inertia_disk)
 %</part1>
 
 
