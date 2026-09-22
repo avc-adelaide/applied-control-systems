9c7ed193a595e7be32432423b77d04a5de95ab1f
Will Robertson <will@wspr.io>
2026-09-22 16:06:53 +0930

    Updates from Overleaf

--- pracs/prac6.m
+++ pracs/prac6.m
@@ -1,9 +1,13 @@
 
 %<*part1>
 % Plot Bode and Nyquist 
-myTF = tf(<NUMERATOR>, <DENOMINATOR>)
-BoFig = figure; bode(myTF); grid on;
-NyFig = figure; nyquist(myTF); grid on;
+pendulum_TF = tf(<NUMERATOR>, <DENOMINATOR>)
+LPfilter_TF = tf(<NUMERATOR>, <DENOMINATOR>)
+BoFig = figure(1);clf; bode(pendulumTF); grid on;
+NyFig = figure(2);clf; nyquist(pendulumTF); grid on
+myTF = pendulum_TF*LPfilter_TF
+BoFig = figure(3);clf; bode(myTF); grid on;
+NyFig = figure(4);clf; nyquist(myTF); grid on;
 %</part1>
 
 
@@ -15,11 +19,14 @@ for i = 1:length(FreqRange)
 	Freq_i = FreqRange(i)
 	% Run Simulation
 	set_param(gcs,'SimulationCommand','start') 
-	% Pause script to allow simulation to run
-	pause(11)
+	% Continously poll the simulation status to check if finished
+        while ~strcmp(get_param(ModelName,'SimulationStatus'),'stopped')
+            pause(0.1)
+        end
 	% Extract data (excluding 1st half transient effects)
-	outputData = <NAME OF OUTPUT SIGNAL>.signals.values;
-	inputData = <NAME OF INPUT SIGNAL>.signals.values;
+        midpoint = floor(length(<ANY SIGNAL>.time)/2);
+	outputData = <NAME OF OUTPUT SIGNAL>.signals().values(midpoint:end);
+	inputData = <NAME OF INPUT SIGNAL>.signals().values(midpoint:end);
    	% Calculate amplitude:
    	pks = findpeaks(outputData);
     	trphs = -findpeaks(-outputData);
@@ -35,9 +42,9 @@ for i = 1:length(FreqRange)
     phaseOffset = mod(phaseOffset,2*pi)-2*pi; % modulo phase 
     phase(i) = phaseOffset*180/pi; % convert to degrees
 end
-
+% These might be useful :) [amp.*cosd(phase),  amp.*sind(phase)]
 % Replot Bode
-BoFig = figure;
+BoFigLP = figure;
 omega = logspace(0,2,101);
 [m,p] = bode(myTF,omega);
 subplot(2,1,1);
@@ -48,9 +55,9 @@ semilogx(omega,p(:));
 grid on;
 
 % Plotting results 
-figure(BoFig); subplot(2,1,1); hold on; semilogx(<FREQUENCY>,<MAGNITUDE>,'ro');
-figure(BoFig); subplot(2,1,2); hold on; semilogx(<FREQUENCY>,<PHASE>,'ro');
-figure(NyFig); hold on; plot(<REAL PART>,<IMAGINARY PART>,'ro');
+figure(BoFigLP); subplot(2,1,1); hold on; semilogx(<FREQUENCY>,<MAGNITUDE>,'r-o');
+figure(BoFigLP); subplot(2,1,2); hold on; semilogx(<FREQUENCY>,<PHASE>,'r-o');
+figure(NyFigLP); hold on; plot(<REAL PART>,<IMAGINARY PART>,'r-o');
 
 %</part3>
 
